576240d722d475fc50627ae514a6f9f9954fecd2
shivam chaturvedi <chaturvedishivam598@gmail.com>
2024-07-17 18:41:43 +0530

    vercel

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,3 @@
-python3.12.3 -m pip install -r requirements.txt
-python3.12.3 manage.py collectstatic --noinput --clear
-python3.12.3 manage.py runserver
+python3.12 -m pip install -r requirements.txt
+python3.12 manage.py collectstatic --noinput --clear
+python3.12 manage.py runserver
